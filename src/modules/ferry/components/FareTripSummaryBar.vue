766c746f5a0885520fc36989d3a5c0012df609b3
Kevin Jimenez <orlando.kj24@gmail.com>
2026-05-15 08:54:31 -0500

    feat: ✨  enhance layout and responsiveness across multiple components

--- src/modules/ferry/components/FareTripSummaryBar.vue
+++ src/modules/ferry/components/FareTripSummaryBar.vue
@@ -1,20 +1,20 @@
 <template>
   <div
-    class="w-full flex shadow-sm rounded-md p-7 gap-y-5 border-2 border-gray-200 justify-between items-center bg-white"
+    class="w-full flex flex-col sm:flex-row shadow-sm rounded-sm p-5 sm:p-7 sm:gap-y-5 border-2 border-gray-200 justify-between items-center bg-white"
   >
-    <div class="flex justify-center items-center gap-x-5">
+    <div class="flex flex-col sm:flex-row justify-center items-center gap-y-3 sm:gap-x-5">
       <TripTypeBadges :is-round-trip="isRoundTrip" />
 
-      <div class="flex text-primary items-center gap-x-1 justify-between">
+      <div class="flex text-primary items-center sm:gap-x-1 sm:justify-between">
         <h6 class="text-h6 font-bold">{{ departureTime }}</h6>
         <ArrowRightDashedIcon class="size-6 text-ink-500" />
         <h6 class="text-h6 font-bold">{{ arrivalTime }}</h6>
       </div>
 
-      <BaseDivider class="rotate-180 h-10 self-center" />
+      <BaseDivider class="rotate-270 sm:rotate-180 h-5 sm:h-10 self-center" />
 
       <div class="flex flex-col gap-y-1">
-        <div class="flex text-primary items-center gap-x-1 w-full justify-between">
+        <div class="flex text-primary items-center gap-y-1 sm:gap-x-1 w-full sm:justify-between">
           <span class="text-xs font-semibold">{{ origin }}</span>
           <ArrowRightDashedIcon class="size-5" />
           <span class="text-xs font-semibold">{{ destination }}</span>
@@ -23,8 +23,8 @@
       </div>
     </div>
 
-    <div class="flex">
-      <span class="flex items-center text-xs font-bold text-heading me-3">
+    <div class="flex flex-col sm:flex-row">
+      <span class="flex items-center text-3xs sm:text-xs font-bold text-heading me-3">
         <span class="flex w-2.5 h-2.5 bg-primary rounded-full me-1.5 shrink-0" />
         {{ routeType }} {{ duration }}
       </span>
--- src/modules/ferry/components/FerryNavHeader.vue
+++ src/modules/ferry/components/FerryNavHeader.vue
@@ -1,11 +1,11 @@
 <template>
-  <header class="w-full flex flex-col px-10 gap-y-8 pt-10">
+  <header class="w-full flex flex-col px-8 sm:px-10 gap-y-4 sm:gap-y-8 pt-6 sm:pt-10">
     <div class="flex items-center flex-1 gap-x-1 cursor-default" @click="emit('back')">
       <ArrowRightDashedIcon class="rotate-180 size-7" />
       <span class="text-sm font-medium">Volver</span>
     </div>
-    <h1 class="text-h1 font-extrabold">{{ title }}</h1>
-    <span v-if="subtitle" class="text-md text-ink-500 font-secondary-italic">{{ subtitle }}</span>
+    <h1 class="text-xl sm:text-h1 font-extrabold">{{ title }}</h1>
+    <span v-if="subtitle" class="text-xs sm:text-md text-ink-500 font-secondary-italic">{{ subtitle }}</span>
   </header>
 </template>
 
